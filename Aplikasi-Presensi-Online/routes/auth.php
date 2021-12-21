3f1edd43d77fc2bf04a628f6710b051925795bb4
Drian-key <riskidrian1@gmail.com>
2021-12-21 11:26:15 +0700

    Menambah seed dan migrasi login auth

---
+++ Aplikasi-Presensi-Online/routes/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+                ->middleware('guest')
+                ->name('login');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('guest');
+
+Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+                ->middleware('auth')
+                ->name('password.confirm');
+
+Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+                ->middleware('auth');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->middleware('auth')
+                ->name('logout');
